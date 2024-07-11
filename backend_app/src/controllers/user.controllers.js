4ac13b72279fe025127fbcc64224cb6dcf45f908
programmingElements <159023430+programmingElements@users.noreply.github.com>
2024-07-11 04:35:28 +0000

    register app and completed utils like async, apiresponse, apierror

--- backend_app/src/controllers/user.controllers.js
+++ backend_app/src/controllers/user.controllers.js
@@ -1,9 +1,21 @@
+import { User } from "../models/user.models.js";
+
 const signup = async (request, response) => {
     try {
-        console.log(request.body);
-        response.status(200).json({ message: 'Welcome to signup page.'});
+        
+        const { username, email, phone, password } = request.body;
+
+        const user = await User.findOne({ $or : [{ username }, { email }]});
+
+        if (user) {
+            throw new Error("User Already Exists.");
+        }
+
+        const newUser = await User.create({ username, email, phone, password });
+
+        response.status(200).json({ message: 'signup successfully completed.', user: newUser });
     } catch (error) {
-        console.log(error);
+        response.status(500).json({err: error.message})
     }
 }
 
