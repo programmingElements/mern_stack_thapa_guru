a37bc5b63931aecf8a9658904c2f84b8ae6ae1c8
programmingElements <159023430+programmingElements@users.noreply.github.com>
2024-07-11 08:16:51 +0000

    asyncHandler used to controllers

--- backend_app/src/controllers/user.controllers.js
+++ backend_app/src/controllers/user.controllers.js
@@ -1,6 +1,9 @@
 import { User } from "../models/user.models.js";
+import {asyncHandler} from "../utils/asynHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import {ApiError} from "../utils/ApiError.js";
 
-const signup = async (request, response) => {
+const signup = asyncHandler(async (request, response) => {
     try {
         
         const { username, email, phone, password } = request.body;
@@ -8,16 +11,16 @@ const signup = async (request, response) => {
         const user = await User.findOne({ $or : [{ username }, { email }]});
 
         if (user) {
-            throw new Error("User Already Exists.");
+            throw new ApiError(400, "User Already Exists.");
         }
 
         const newUser = await User.create({ username, email, phone, password });
 
-        response.status(200).json({ message: 'signup successfully completed.', user: newUser });
+        response.status(200).json(new ApiResponse(201, newUser, "signup completed successfully."));
     } catch (error) {
-        response.status(500).json({err: error.message})
+        response.status(error.statusCode || 500).json(error)
     }
-}
+})
 
 const login = async (request, response) => {
     try {
--- backend_app/src/utils/ApiError.js
+++ backend_app/src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         super(message);
         this.statusCode = statusCode;
         this.data = null;
-        this.message = message;
+        this.msg = message;
         this.success = false;
         this.errors = errors;
 
