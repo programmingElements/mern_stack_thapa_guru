a1df13c357753e7d24956491a250d68f68fb7fd9
programmingElements <159023430+programmingElements@users.noreply.github.com>
2024-07-12 10:00:56 +0000

    error handling part 2

--- backend_app/src/middlewares/validate.middleware.js
+++ backend_app/src/middlewares/validate.middleware.js
@@ -6,8 +6,10 @@ const validate = (schema) => async (request, response, next) => {
         request.body = parseBody;
         next();
     } catch (error) {
+        const msg = error.issues[0].message;
+        next(new ApiError(400, msg));
         // response.status(400).json({ msg: error });
-        next(new ApiError(400, "zod validation error.", error));
+        // next(new ApiError(400, "zod validation error.", error));
     }
 }
 
